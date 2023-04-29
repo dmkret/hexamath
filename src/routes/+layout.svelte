eac537a9995dc25b162c951a2c8f169e7b4cf870
dmkret <8580466+dmkret@users.noreply.github.com>
2023-04-29 22:12:18 +0300

    initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,40 @@
+<script>
+	import '@fontsource/share-tech-mono/400.css';
+</script>
+
+<svelte:head>
+	<title>Hexamath</title>
+	<meta name="description" content="Hexadecimal math trainer" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<meta name="color-scheme" content="light dark" />
+</svelte:head>
+
+<main>
+	<slot />
+</main>
+
+<style lang="scss">
+	:global(*) {
+		box-sizing: border-box;
+		-webkit-tap-highlight-color: transparent;
+		font-family: 'Share Tech Mono', monospace;
+	}
+
+	:global(html, body) {
+		height: 100%;
+		margin: 0;
+		padding: 0;
+
+		font-size: 38px;
+		background: #fff;
+
+		@media (prefers-color-scheme: dark) {
+			background: #202124;
+		}
+	}
+
+	main {
+		height: 100%;
+		display: grid;
+	}
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,109 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	let answer = '';
+	let task: null | {
+		sign: string;
+		left: number;
+		right: number;
+		result: number;
+	};
+
+	$: answer, check_answer();
+
+	const handle_keydown = (e: KeyboardEvent) => {
+		if (e.code === 'Backspace') {
+			answer = answer.slice(0, answer.length - 1);
+			return;
+		}
+
+		if (e.repeat) return;
+
+		const upper_key = e.key.toUpperCase();
+		if (!/^[0-9A-F]+$/.test(upper_key) && !(upper_key === '-' && answer.length === 0)) return;
+
+		answer += upper_key;
+	};
+
+	const get_number = () => Math.trunc(Math.random() * 0xf);
+
+	const generate_task = () => {
+		const signs = ['-', '+', '*', '/'];
+		const sign = signs[Math.trunc(Math.random() * signs.length)];
+
+		let left = 0;
+		let right = 0;
+		let result = 0;
+
+		if (sign === '/') {
+			result = get_number();
+			right = get_number();
+			left = result * right;
+		} else {
+			left = get_number();
+			right = get_number();
+			result = eval(left + sign + right);
+		}
+
+		task = { sign, left, right, result };
+	};
+
+	const check_answer = () => {
+		if (!task) return;
+
+		if (answer !== task.result.toString(16).toUpperCase()) return;
+
+		answer = '';
+		generate_task();
+	};
+
+	onMount(() => generate_task());
+</script>
+
+<section>
+	<div class="task">
+		{#if task}
+			{task.left.toString(16).toUpperCase()}
+			{task.sign}
+			{task.right.toString(16).toUpperCase()}
+		{/if}
+	</div>
+
+	<div class="answer">
+		{answer}
+	</div>
+</section>
+
+<svelte:window on:keydown={handle_keydown} />
+
+<style lang="scss">
+	section {
+		height: 100%;
+		text-align: center;
+
+		display: grid;
+		row-gap: 24px;
+		justify-items: stretch;
+		grid-template-rows: 1fr auto 1fr;
+	}
+
+	.task {
+		pointer-events: none;
+		margin-bottom: 1rem;
+		align-self: flex-end;
+
+		color: #000;
+		@media (prefers-color-scheme: dark) {
+			color: #fff;
+		}
+	}
+
+	.answer {
+		height: 1rem;
+
+		color: #4d5156;
+		@media (prefers-color-scheme: dark) {
+			color: #bdc1c6;
+		}
+	}
+</style>
